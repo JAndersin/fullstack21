87d2864c855338b23b6ece3f482fdc1396220866
Maun0 <maunothegnome@gmail.com>
2021-02-01 21:37:11 +0200

    Osa1

---
+++ Osa1/unicafe/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const Statistics = (props) => {  
+  if (props.goods > 0 || props.neutrals > 0 || props.bads > 0 ) {
+    return (
+    <div>
+      <p>Good: {props.goods}</p>
+      <p>Neutral: {props.neutrals}</p>
+      <p>Bad: {props.bads}</p>
+    </div>
+    )
+
+  } else {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+      )
+  }
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good+1)}>Good</button><button onClick={() => setNeutral(neutral+1)}>Neutral</button><button onClick={() => setBad(bad+1)}>Bad</button>
+      <h1>Statistics</h1>
+      <Statistics goods={good} neutrals={neutral} bads={bad} />
+    </div>
+  )
+}
+
+export default App
